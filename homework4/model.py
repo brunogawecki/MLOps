65af081e73a0032ad3726a580bcf3939b4d6c26a
Bruno <brunogawecki0@gmail.com>
2025-12-31 15:34:04 +0100

    add saved model, model loading and test inference

---
+++ homework4/model.py
@@ -0,0 +1,46 @@
+import torch
+from transformers import AutoModelForImageClassification, AutoImageProcessor
+from pathlib import Path
+from PIL import Image
+
+MODEL_PATH = Path("MobileNetV2_model")
+
+def load_model():
+    image_processor = AutoImageProcessor.from_pretrained(MODEL_PATH)
+    model = AutoModelForImageClassification.from_pretrained(MODEL_PATH)
+    model.eval()
+    return model, image_processor
+
+def predict(image_path: Path, model, image_processor):
+    image = Image.open(image_path)
+    if image.mode != 'RGB':
+        image = image.convert('RGB')
+    
+    inputs = image_processor(image, return_tensors="pt")
+    
+    with torch.no_grad():
+        outputs = model(**inputs)
+        logits = outputs.logits
+    
+    # Get top 5 predictions
+    probabilities = torch.nn.functional.softmax(logits, dim=-1)[0]
+    top5_probs, top5_indices = torch.topk(probabilities, 5)
+    
+    results = []
+    for prob, idx in zip(top5_probs, top5_indices):
+        class_id = int(idx.item())
+        results.append({
+            'class_id': class_id,
+            'class_name': model.config.id2label[class_id],
+            'score': float(round(prob.item(), 4))
+        })
+    
+    return results
+
+if __name__ == "__main__":
+    model, image_processor = load_model()
+    results = predict(Path("cow.jpg"), model, image_processor)
+    top5_preds = results[:5]
+    print(f'')
+    for pred in top5_preds:
+        print(f"Class ID: {pred['class_id']}, Class Name: {pred['class_name']}, Score: {pred['score']}")
